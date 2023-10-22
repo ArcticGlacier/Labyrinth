c598fb99ae84903b129430a0af57f05821c7267f
Gagan <sweetpapaya001@gmail.com>
2023-10-22 17:48:32 -0600

    balls moving

--- src/labyrinth.js
+++ src/labyrinth.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./labyrinth.css";
+import { useEffect } from "react";
 export default function Labyrinth() {
+  const [ballCoords, setBallCoords] = useState({ row: 9, column: 5 });
+
   // 1 represents walls, 0 is path, 3 is holes, 2 is ball, 4 is winning hole
-  let labyrinth = [
+  const [labyrinth, setLabyrinth] = useState([
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 3, 0, 1, 0, 1, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 1, 4, 1, 0, 1],
@@ -12,32 +16,66 @@ export default function Labyrinth() {
     [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
     [1, 3, 1, 0, 0, 0, 0, 1, 3, 1],
     [1, 1, 1, 1, 1, 2, 1, 1, 1, 1],
-  ];
+  ]);
 
-  function getLabyrinthClass(cell) {
-    if (cell === 0) {
-      return "path";
-    } else if (cell === 1) {
-      return "wall";
-    } else if (cell === 2) {
-      return "ball";
-    } else if (cell === 3 || cell === 4) {
-      return "hole";
-    }
+  function moveBallUp() {
+    let maze = labyrinth;
+    maze[ballCoords.row][ballCoords.column] = 0;
+    maze[ballCoords.row - 1][ballCoords.column] = 2;
+    setLabyrinth(maze);
+    setBallCoords({ row: ballCoords.row - 1, column: ballCoords.column });
   }
 
+  function moveBallDown() {
+    let maze = labyrinth;
+    maze[ballCoords.row][ballCoords.column] = 0;
+    maze[ballCoords.row + 1][ballCoords.column] = 2;
+    setLabyrinth(maze);
+    setBallCoords({ row: ballCoords.row + 1, column: ballCoords.column });
+  }
+
+  function moveBallLeft() {
+    let maze = labyrinth;
+    maze[ballCoords.row][ballCoords.column] = 0;
+    maze[ballCoords.row][ballCoords.column - 1] = 2;
+    setLabyrinth(maze);
+    setBallCoords({ row: ballCoords.row, column: ballCoords.column - 1 });
+  }
+
+  function moveBallRight() {
+    let maze = labyrinth;
+    maze[ballCoords.row][ballCoords.column] = 0;
+    maze[ballCoords.row][ballCoords.column + 1] = 2;
+    setLabyrinth(maze);
+    setBallCoords({ row: ballCoords.row, column: ballCoords.column + 1 });
+  }
+
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp") {
+      moveBallUp();
+    } else if (event.key === "ArrowDown") {
+      moveBallDown();
+    } else if (event.key === "ArrowLeft") {
+      moveBallLeft();
+    } else if (event.key === "ArrowRight") {
+      moveBallRight();
+    }
+  });
+
   return (
-    <table cellspacing="0" cellpadding="0">
-      {labyrinth.map((row, rowIndex) => (
-        <tr key={rowIndex}>
-          {row.map((cell, colIndex) => (
-            <td
-              key={colIndex}
-              className={`row-${rowIndex} column-${colIndex} cell-${cell}`}
-            ></td>
-          ))}
-        </tr>
-      ))}
+    <table cellSpacing="0" cellPadding="0">
+      <tbody>
+        {labyrinth.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, colIndex) => (
+              <td
+                key={colIndex}
+                className={`row-${rowIndex} column-${colIndex} cell-${cell}`}
+              ></td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
